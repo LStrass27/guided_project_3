ea7b26e9eb1fddbef6e0fc4d33731d814d7c405e
Rishita Palle <rishitapalle@gmail.com>
2025-07-09 10:45:35 -0400

    new

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+import sqlite3
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
 
 app = Flask(__name__)
+
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, columns_to_drop):
+        self.columns_to_drop = columns_to_drop
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Make a copy to avoid modifying the original data
+        X_transformed = X.copy()
+        # Drop the specified columns
+        return X_transformed.drop(columns=self.columns_to_drop, errors='ignore')
 
 with open('trained_model.pkl', 'rb') as file:
     model = pickle.load(file)
